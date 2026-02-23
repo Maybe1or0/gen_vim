e5b6d954c275c5506479d9dc5d654dea86a6ce57
patrick.holtzmann <patrick.holtzmann@epita.fr>
2026-02-23 20:00:42 +0100

    Genvim update

--- genvimSetup/setup.sh
+++ genvimSetup/setup.sh
@@ -1,9 +1,9 @@
+#Back-up (au cazou)
 cp ~/.bashrc bashrc.old
 cp ~/.config/i3/config config.old
 
-# echo $(cat bashgen) >> test
-# echo $(cat i3gen) >> nvtest
 
 cat bashgen >> test
 cat i3gen >> nvtest
-mv genvim-layout.json ~/.config/i3/
+cp genvim-layout.json ~/.config/i3/
+cp brainrotvideos ../ 
