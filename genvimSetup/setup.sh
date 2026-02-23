6defdb858e7fd8ab9c3a41391b6ee8d93cff43a0
patrick.holtzmann <patrick.holtzmann@epita.fr>
2026-02-23 21:40:01 +0100

    Genvim update line

--- genvimSetup/setup.sh
+++ genvimSetup/setup.sh
@@ -4,6 +4,7 @@ cp ~/.config/i3/config config.old
 
 
 cat bashgen >> ~/.bashrc
+sed -i '/^bindsym \$mod+w/d' ~/.config/i3/config
 cat i3gen >> ~/.config/i3/config
 cp genvim-layout.json ~/.config/i3/
-cp brainrotvideos ../ 
+cp -r brainrotvideos ../../ 
