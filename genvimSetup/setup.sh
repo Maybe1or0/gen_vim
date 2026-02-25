a439beb2a3b7b1847fca2f71072f91a091102c52
patrick.holtzmann <patrick.holtzmann@epita.fr>
2026-02-25 14:17:19 +0100

    genvim setup repared

--- genvimSetup/setup.sh
+++ genvimSetup/setup.sh
@@ -7,7 +7,7 @@ cat bashgen >> ~/.bashrc
 sed -i '/^bindsym \$mod+w/d' ~/.config/i3/config
 cat i3gen >> ~/.config/i3/config
 cp genvim-layout.json ~/.config/i3/
-cp -r ~/afs brainrotvideos
+cp -r brainrotvideos ~/afs/
 
 source ~/.bashrc
 i3-msg reload
